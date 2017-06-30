552d507961eea6c01ffedd42f19201f3f88e1314
Theo <theo.despoudis@teckro.com>
2017-06-30 17:00:26 +0100

    Code cleanup

--- src/MyEditor.js
+++ src/MyEditor.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactQuill from 'react-quill';
 import '../node_modules/quill/dist/quill.snow.css';
 
@@ -23,102 +23,100 @@ const CustomToolbar = () => (
       <option selected></option>
     </select>
     <button className="ql-vimeo">
-        <span className="toolbar-space">Vimeo</span>
+      <span className="toolbar-space">Vimeo</span>
     </button>
     <button className="ql-dailyMotion">
-        <span className="toolbar-space" >Dailymotion</span>
+      <span className="toolbar-space">Dailymotion</span>
     </button>
-    <button className="ql-youtube" >
-      <span className="toolbar-space" >Youtube</span>
+    <button className="ql-youtube">
+      <span className="toolbar-space">Youtube</span>
     </button>
     <button className="ql-facebook">
-       <span className="toolbar-space">Facebook</span>
+      <span className="toolbar-space">Facebook</span>
     </button>
   </div>
-)
+);
 
 /**
  * Basic Editor
  */
 class MyEditor extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state={text:''}
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
 
-        var self=this;//Usefull to bind the method
-        this.modules = {
-          toolbar: {
-            container: "#toolbar",
-            'image-tooltip': true,
-            'link-tooltip': true,
-            handlers:{
-              dailyMotion: this.insertDailyMotion.bind(self),
-              youtube: this.insertYoutube.bind(self),
-              vimeo: this.insertVimeo.bind(self),
-              facebook: this.insertFacebook.bind(self)
-            }
+    var self = this;
+    this.modules = {
+      toolbar: {
+        container: "#toolbar",
+        'image-tooltip': true,
+        'link-tooltip': true,
+        handlers: {
+          dailyMotion: this.insertDailyMotion.bind(self),
+          youtube: this.insertYoutube.bind(self),
+          vimeo: this.insertVimeo.bind(self),
+          facebook: this.insertFacebook.bind(self)
+        }
+      }
+    }
+
+    this.reactQuillRef = null;
+  }
+
+  onTextChange(value) {
+    this.setState({text: value})
+  }
+
+  insertVideo(video) {
+    const editor = this.reactQuillRef.getEditor();
+    const delta = [
+      {
+        insert: {
+          video: video,
+          attributes: {
+            width: 900,
+            height: 900
           }
         }
+      },
+    ];
+    editor.updateContents(delta);
+  }
 
-        this.reactQuillRef=null;
-    }
+  insertDailyMotion() {
+    this.insertVideo("//www.dailymotion.com/embed/video/x5rx12e");
+  }
 
-    onTextChange(value) { 
-        this.setState({text:value})
-    }
+  insertYoutube() {
+    this.insertVideo("https://www.youtube.com/embed/1rDQccuLEvY?ecver=2");
+  }
 
-    insertVideo(video) {
-        const editor = this.reactQuillRef.getEditor();
-        const index = editor.getSelection().index || 0;
-        // editor.insertEmbed(index, 'video', video);
-        // editor.format('width',1000);
-        // editor.format('height',1000);
-       const delta= [
-         {
-           insert: {
-            video: video,
-            attributes: {
-              width: 900,
-              height: 900
-            }
-         }
-        },
-       ]
-       editor.updateContents(delta);
-    }
+  insertVimeo() {
+    this.insertVideo("https://player.vimeo.com/video/221105534");
+  }
 
-    insertDailyMotion(){
-      this.insertVideo("//www.dailymotion.com/embed/video/x5rx12e");
-    }
+  insertFacebook() {
+    this.insertVideo("https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fenikosgr%2Fposts%2F1427262130644621&width=500")
+  }
 
-    insertYoutube(){
-      this.insertVideo("https://www.youtube.com/embed/1rDQccuLEvY?ecver=2");
-    }
-
-    insertVimeo(){
-      this.insertVideo("https://player.vimeo.com/video/221105534");
-    }
-
-    insertFacebook(){
-      this.insertVideo("https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fenikosgr%2Fposts%2F1427262130644621&width=500")
-    }
-
-    render(){
-      return (
-        <div>
-         <CustomToolbar />
-         <ReactQuill
-            ref = { (el) => { this.reactQuillRef = el; } }
-            value = {this.state.body}
-            onChange = {this.handleChange}
-            modules = {this.modules}
-            formats = {MyEditor.formats}
-            theme="snow"
-          />
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <CustomToolbar />
+        <ReactQuill
+          ref={ (el) => {
+            this.reactQuillRef = el;
+          } }
+          value={this.state.body}
+          onChange={this.handleChange}
+          modules={this.modules}
+          formats={MyEditor.formats}
+          theme="snow"
+        />
+      </div>
+    )
+  }
 
 }
 
@@ -126,7 +124,7 @@ MyEditor.formats = [
   'header', 'font', 'size',
   'bold', 'italic', 'underline', 'strike', 'blockquote',
   'list', 'bullet', 'indent',
-  'link', 'image', 'color','script','video'
-]
+  'link', 'image', 'color', 'script', 'video'
+];
 
 export default MyEditor;
